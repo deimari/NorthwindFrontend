8aa99fc5888f2e0cea1893221f5816d8a149513b
deimari <lgtbozdogan@gmail.com>
2021-04-18 20:25:28 +0300

    .

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -5,19 +5,32 @@ import { CategoryService } from 'src/app/services/category.service';
 @Component({
   selector: 'app-category',
   templateUrl: './category.component.html',
-  styleUrls: ['./category.component.css']
+  styleUrls: ['./category.component.css'],
 })
 export class CategoryComponent implements OnInit {
   categories: Category[] = [];
-  constructor(private categoryService: CategoryService) { }
+  currentCategory: Category;
+  constructor(private categoryService: CategoryService) {}
 
   ngOnInit(): void {
-    this.getCategories()
+    this.getCategories();
   }
-  
+
   getCategories() {
-    this.categoryService.getCategories().subscribe(response => {
+    this.categoryService.getCategories().subscribe((response) => {
       this.categories = response.data;
-    })
+    });
+  }
+
+  setCurrentCategory(category: Category) {
+    this.currentCategory = category;
+  }
+
+  getCategoryClass(category: Category) {
+    if (category == this.currentCategory) {
+      return 'list-group-item active';
+    } else {
+      return 'list-group-item';
+    }
   }
 }
